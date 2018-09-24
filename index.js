6e4fed2ff83742fdd7c2928bc6f650caf9e1bac4
Eric Abenojar <abenoeric@gmail.com>
2018-09-24 18:44:46 -0500

    Update error syntax

--- index.js
+++ index.js
@@ -5,50 +5,51 @@ var app = express();
 //Set view engine for node.js
 app.set("view engine", "ejs");
 //Tell node where the css files are located
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 // app.use(express.static("public"));
 
 //HOME PAGE
-app.get("/", function(req,res){
+app.get("/", function(req, res) {
   res.render("index");
 });
 //ABOUT PAGE
-app.get("/about", function(req,res){
-  res.render("about")
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 //PORTFOLIO PAGE
-app.get("/portfolio", function(req,res){
-  res.render("portfolio")
+app.get("/portfolio", function(req, res) {
+  res.render("portfolio");
 });
 // SKILLS PAGE
-app.get("/skills", function(req,res){
-  res.render("skills")
+app.get("/skills", function(req, res) {
+  res.render("skills");
 });
 // CONTACT PAGE
-app.get("/contact", function(req,res){
-  res.render("contact")
+app.get("/contact", function(req, res) {
+  res.render("contact");
 });
 
 var port = process.env.PORT || 8080;
 
-app.listen(port, console.log("APP STARTED! on port" + port))
+app.listen(port, console.log("APP STARTED! on port" + port));
 
-const ADD = 'ADD';
+// const ADD = 'ADD';
 
-function addMessage(){
-    return {
-        type: ADD,
-        message: 'HEllo'
-    }
+// function addMessage(){
+//     return {
+//         type: ADD,
+//         message: 'HEllo'
+//     }
+// }
+
+function messageReducer(state = [], action) {
+  switch (action.type) {
+    case ADD:
+      return {
+        ...state,
+        message: action.message
+      };
+  }
 }
 
-function messageReducer(state =[],action){
-    switch(action.type){
-        case ADD:
-        return {
-            ...state, message: action.message
-        }
-    }
-}
-
-store.dispatch(addMessage())
+// store.dispatch(addMessage())
